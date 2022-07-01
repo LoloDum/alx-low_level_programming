4e979d6d5f217cb5a3944fa80f1775369957ccbd
LoloDum <chinedumokamgba@gmail.com>
2022-06-30 18:11:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,21 @@
-#!/bin/bash
-gcc -save-temps 4-puts.c -o 4-puts
+#include <stdio.h>
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,1 @@
-a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line
+ a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line
