bd380db9795b4fdc5d8541d9a270afeee724e061
LoloDum <chinedumokamgba@gmail.com>
2022-07-05 07:59:04 -0700

    putchar(n);

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,9 +7,12 @@
  */
 int main(void)
 {
-		char low;
+	char low;
 
-		for (low = 'a'; low <= 'z'; low++)
-			putchar(low);
-		return (1);
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+
+	putchar('\n');
+
+	return (0);
 }
