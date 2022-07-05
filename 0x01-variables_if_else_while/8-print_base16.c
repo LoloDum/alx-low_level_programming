cf8fdd5031995dd6e4f972c3543656bbc582660d
LoloDum <chinedumokamgba@gmail.com>
2022-07-05 05:54:00 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,20 @@
-#incude <stdio.h>
-/**
- * main - Prints all the numbers of base 16 in lowercase
- *
- * Return: Always 0
- */
-int main(void)
-{
-	int a;
-	char l;
+#incude <stdio.h>                                                                                                                                        
+/**                                                                                                                                                       * main - Prints all the numbers of base 16 in lowercase                                                                                                 
+ *                                                                                                                                  
+ * Return: Always 0                                                                                                                                      
+ */                                                                                                                                                      
+int main(void)                                                                                                                                           
+{                                                                                                                                                        
 
-	for (a = 0; a < 10; a++)
-		putchar((a % 10) + '0');
-
-	for (l = 'a'; l <= 'f'; l++)
-		putchar(l);
-
-	putchar('\n');
+        int a;                                                                                                                                           
+        char l;                                                                                                                                          
+                                                                                                                                                
+        for (a = 0; a < 10; a++)                                                                                                                         
+                putchar((a % 10) + '0');                                                                                                                 
+                                                                                                                                    
+        for (l = 'a'; l <= 'f'; l++)                                                                                                                     
+                putchar(l);                                                                                                                                                                                                                                                                                
+        putchar('\n');                                                                                                                                
 
 	return (0);
 }
