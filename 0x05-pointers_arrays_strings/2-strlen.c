0fb42a47614521040e4c85c70b93d069e2196341
LoloDum <chinedumokamgba@gmail.com>
2022-07-14 06:43:27 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,9 +2,9 @@
 
 /**
  * _strlen - Returns the length of a string
- * @str: The string to get the length of
+ * @s: The string to get the length of
  *
- * Return: length of the @str
+ * Return: length of the @s
  */
 int _strlen(char *s)
 {
