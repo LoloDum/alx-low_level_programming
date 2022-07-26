5f30da0ca20d974b356c95dedda22b9923eac201
LoloDum <chinedumokamgba@gmail.com>
2022-07-26 10:42:39 -0700

    2-str_covcat.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Returns a pointer to a newly-allocated space in memory
+ * containing a copy of the string given as parameter
+ * @str: The string to be copied
+ *
+ * Return: If str == NULL or insufficient memory is available - NULL.
+ * Otherwise - a pointer to the duplicated string.
+ */
+char *_strdup(char *str)
+{
+	char *duplicate;
+	int ind, len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (ind = 0; str[ind]; ind++)
+		len++;
