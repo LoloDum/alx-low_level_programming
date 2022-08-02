c3d406da16719dd97f369c78ff0678973c5ab201
LoloDum <chinedumokamgba@gmail.com>
2022-08-02 09:54:30 -0700

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -4,12 +4,13 @@
 /**
  * free_dog - Frees dogs
  * @d: The dog to be freed
- * Return: 0
  */
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
 		return;
 
+	free(d->owner);
+	free(d->name);
 	free(d);
 }
